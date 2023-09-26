74b2dd45b183921dfefb43774933cce431218ec7
TkBow <thebow25@gmail.com>
2023-09-25 23:24:25 -0600

    Got the ball being able to bounce and move

--- BrickBreakerGame/Assets/Scripts/BallBehaviour.cs
+++ BrickBreakerGame/Assets/Scripts/BallBehaviour.cs
@@ -8,8 +8,9 @@ using Random = System.Random;
 
 public class BallBehaviour : MonoBehaviour
 {
-    
+    // Helped made this scripted with this https://www.youtube.com/watch?v=RYG8UExRkhA Specifically and only with the ball movement chapter
     private Rigidbody rb;
+    public float speed = 500f;
 
     private void Awake()
     {
@@ -20,7 +21,17 @@ public class BallBehaviour : MonoBehaviour
 
     private void Start()
     {
+        
+        Invoke(nameof(SetRandomTrajectory), 1f);
+        
+    }
+
+    private void SetRandomTrajectory()
+    {
         Vector3 force = Vector3.zero;
-        force.x = 1f;
+        force.x = UnityEngine.Random.Range(-1f, 1f); // Fixed it with UnityEngine
+        force.y = -1f;
+        
+        this.rb.AddForce(force.normalized * this.speed);
     }
 }
